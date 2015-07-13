431ca87fd5d2957cba0868c2dd641da7fa311da5
Abhishek Dey <abhishekdey.sdm@gmail.com>
2015-07-13 20:31:18 +0530

    First changes

---
+++ server/start.js
@@ -0,0 +1,8 @@
+//http://stackoverflow.com/questions/22213980/could-someone-explain-what-process-argv-means-in-node-js-please
+//https://github.com/clvv/scriptbroadcast/blob/master/bin/scriptbroadcast
+
+var PORT = process.argv[2] && parseInt(process.argv[2], 10) || 3000;
+var STATIC_DIR = __dirname + '/../app';
+var TEST_DIR = __dirname + '/../test';
+var DATA_FILE = __dirname + '/data/restaurants.json';
+require('./index').start(PORT, STATIC_DIR, DATA_FILE, TEST_DIR);
