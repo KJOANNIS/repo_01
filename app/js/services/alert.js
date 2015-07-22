3d920284b439268daed68cfaee320d21cb5e78e3
Abhishek Dey <abhishekdey.sdm@gmail.com>
2015-07-22 13:07:32 +0530

    Adding points to FAQ

--- app/js/services/alert.js
+++ app/js/services/alert.js
@@ -1,3 +1,3 @@
-'use strict';
+    'use strict';
 
 foodMeApp.value('alert', window.alert);
--- app/js/services/cart.js
+++ app/js/services/cart.js
@@ -4,7 +4,10 @@ foodMeApp.service('cart', function Cart(localStorage, customer, $rootScope, $htt
   var self = this;
 
 
+    /*adding the selected item to cart with args (menuItem, restaurant)*/
   self.add = function(item, restaurant) {
+      /*check whether cart is null or not */
+      /*if not add to the restaurant id ,name nad description to cart else not add */
     if (!self.restaurant || !self.restaurant.id) {
       self.restaurant = {
         id: restaurant.id,
@@ -13,35 +16,53 @@ foodMeApp.service('cart', function Cart(localStorage, customer, $rootScope, $htt
       };
     }
 
+     /*either add the item or if all ready there increment the quantity*/
     if (self.restaurant.id == restaurant.id) {
+
+        /*if cart is not empty search for that particular item*/
       self.items.forEach(function(cartItem) {
+
+        /*search for the particular item in the cart and increment*/
         if (item && cartItem.name == item.name) {
+
+          /*just increment the quantity as item is all ready there*/
           cartItem.qty ++;
+
+          //so that it fails in the next if condition
           item = null;
         }
       });
+
+        debugger;
+      /*If item is getting added first time in cart or cart was empty*/
       if (item) {
+        /*https://docs.angularjs.org/api/ng/function/angular.copy*/
         item = angular.copy(item);
         item.qty = 1;
         self.items.push(item);
       }
-    } else {
+    }
+
+    /*mix menu items from different restaurants.*/
+    else {
       alert('Can not mix menu items from different restaurants.');
     }
   };
 
-
+   /*remove item from cart*/
   self.remove = function(item) {
     var index = self.items.indexOf(item);
     if (index >= 0) {
       self.items.splice(index, 1);
     }
+      /*check if items there or not*/
     if (!self.items.length) {
       self.restaurant = {};
     }
   }
 
 
+    /*Total bill after order*/
   self.total = function() {
     return self.items.reduce(function(sum, item) {
       return sum + Number(item.price * item.qty);
@@ -49,6 +70,7 @@ foodMeApp.service('cart', function Cart(localStorage, customer, $rootScope, $htt
   };
 
 
+   /*payment of order*/
   self.submitOrder = function() {
     if (self.items.length) {
       return $http.post('/api/order', {
@@ -63,7 +85,7 @@ foodMeApp.service('cart', function Cart(localStorage, customer, $rootScope, $htt
     }
   }
 
-
+    /**/
   self.reset = function() {
     self.items = [];
     self.restaurant = {};
@@ -79,6 +101,8 @@ foodMeApp.service('cart', function Cart(localStorage, customer, $rootScope, $htt
     var json = localStorage[storageName];
 
     self[localName] = json ? JSON.parse(json) : new Type;
+
+      /*https://docs.angularjs.org/api/ng/type/$rootScope.Scope*/
 
     $rootScope.$watch(
         function() { return self[localName]; },
